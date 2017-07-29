6f35bf8e47ebf416c364eacd1b2c64a0c7f6afd9
Shahrukh Omar <shahrukh.dev@gmail.com>
2017-07-29 16:30:58 +0100

    [test] it requests gist from the correct URL when no user name is provided

--- tests/integration/components/ember-gisty-test.js
+++ tests/integration/components/ember-gisty-test.js
@@ -1,6 +1,11 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Service from '@ember/service';
+import wait from 'ember-test-helpers/wait';
+
+import RSVP from 'rsvp';
+
+const GITHUB_GIST_HOST = 'https://gist.github.com';
 
 moduleForComponent('ember-gisty', 'Integration | Component | ember gisty', {
   integration: true,
@@ -23,4 +28,25 @@ test('it displays error if gist not provided', function(assert) {
     this.$('#error').length,
     1
   );
+});
+
+test('it requests gist from the correct URL when no user name is provided', function(assert) {
+  assert.expect(1);
+
+  const gist = '1234567789';
+
+  this.set('gistFetchService.request', (url) => {
+    assert.equal(
+      url,
+      `anonymous/${gist}.json`
+    );
+
+    return new RSVP.Promise((resolve) => { resolve(); });
+  });
+
+  this.set('gist', gist);
+
+  this.render(hbs`{{ember-gisty gist=gist}}`);
+
+  return wait();
 });
