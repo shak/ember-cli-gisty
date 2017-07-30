974eb0e8cf5296b79fa6f152d416af09d7768c3e
Shahrukh Omar <shahrukh.dev@gmail.com>
2017-07-30 15:54:07 +0100

    [test] it sets the file query param when particular file is required

--- tests/integration/components/ember-gisty-test.js
+++ tests/integration/components/ember-gisty-test.js
@@ -97,6 +97,31 @@ test('it sets the dataType to jsonp always', function(assert) {
   return wait();
 });
 
+test('it sets the file query param when particular file is required', function(assert) {
+  assert.expect(1);
+
+  const gist = '1234567789';
+  const user = 'shahrukhomar';
+  const filename = 'test.js';
+
+  this.set('gistyAjaxService.request', (url, options) => {
+    assert.deepEqual(
+      options.data,
+      { file: filename }
+    );
+
+    return new RSVP.Promise((resolve) => { resolve(); });
+  });
+
+  this.set('gist', gist);
+  this.set('user', user);
+  this.set('filename', filename);
+
+  this.render(hbs`{{ember-gisty user=user gist=gist filename=filename}}`);
+
+  return wait();
+});
+
 test('it yields isLoading when request is being processed', function(assert) {
   assert.expect(1);
 
